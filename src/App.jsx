c75216e6387aba91c6f54da46f8825b1cedb63a3
Saba Antadze <sabaantadze@gmail.com>
2024-03-07 19:04:17 +0400

    add Collaboration, Services and procing sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
       </div>
       <ButtonGradient />
     </>
