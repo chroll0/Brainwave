8e2b719e68f5f97949828b54bb67ce80bbc76a21
Saba Antadze <sabaantadze@gmail.com>
2024-03-07 18:01:59 +0400

    add header, hero and benefits sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import "./App.css";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+      </div>
+      <ButtonGradient />
     </>
   );
-}
+};
 
 export default App;
